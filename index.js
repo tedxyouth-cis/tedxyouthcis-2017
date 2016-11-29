ca3b1367f75b7ecdc421276f5e62ad300812df12
ingridt123 <ingridszenga@gmail.com>
2016-11-29 09:17:45 +0800

    modified position of top button, return to initial

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 window.onscroll = function() {
-	var position = $("#speakers").position();
-	if (document.body.scrollTop < position.top + 100) {
+	var position = $("#sp2").position();
+	if (document.body.scrollTop < position.top) {
 		$("#top").css("display", "none");
 	}
 	else {
