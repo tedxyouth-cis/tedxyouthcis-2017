167d2cfcb9763032f91b9d13d4e3b1479ec53dad
ingridt123 <ingridszenga@gmail.com>
2016-11-30 21:24:21 +0800

    quick fix for viewing on web

--- index.js
+++ index.js
@@ -19,3 +19,20 @@ $("#top").click(function(e) {
 	e.preventDefault();		// prevent page from reloading
 	goToByScroll("#speakers");
 });
+
+
+// show different things on different screens
+$(window).on("load resize", function () {
+	console.log(window.innerWidth);
+	if (window.innerWidth > 850) {
+		alert("View on your phone or decrease the width of your window to view the brochure!");
+		$("#poster").css("display", "initial");
+		$("#container").css("display", "none");
+		$("#title-image").css("display", "none");
+	}
+	else {
+		$("#poster").css("display", "none");
+		$("#container").css("display", "initial");
+		$("#title-image").css("display", "initial");
+	}
+});
